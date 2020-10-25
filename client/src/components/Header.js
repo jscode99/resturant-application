a3c251d63e63a96f14ae9999b0afba8b010538f8
Jishnu <jscode99@gmail.com>
2020-10-25 16:50:31 +0530

    Implemented navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import {Link} from 'react-router-dom'
 
 function Header() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="#">
+          Logo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                Login <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                Signup
+              </Link>
+            </li>
+         
+          </ul>
+     
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
